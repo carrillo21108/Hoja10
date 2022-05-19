cc4289112ccd2fbf0065f725802dc03a7d02e1dc
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2022-05-18 19:33:21 -0600

    Base Hoja de trabajo

---
+++ test/uvg/edu/test/GraphTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package uvg.edu.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Brian Carrillo
+ *
+ */
+class GraphTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
