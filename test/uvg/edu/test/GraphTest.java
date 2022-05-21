62249a34628b326d0feb039ecb55096e5104e42a
lop21666 <91706525+lop21666@users.noreply.github.com>
2022-05-20 22:47:38 -0600

    Pruebas unitarias algoritmoFloyd y centerGraph

--- test/uvg/edu/test/GraphTest.java
+++ test/uvg/edu/test/GraphTest.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 import org.junit.jupiter.api.Test;
 
 import uvg.edu.common.AlgoritmoFloyd;
+import uvg.edu.common.Controller;
 import uvg.edu.common.Graph;
 import uvg.edu.io.Reader;
 
@@ -23,11 +24,11 @@ class GraphTest {
 	@Test
 	void algoritmoFloydTest() {
 		String valorEsperado = 
-				"[0][30][55][70][15]\r\n"
-				+ "[30][0][25][40][40]\r\n"
-				+ "[55][25][0][15][65]\r\n"
-				+ "[70][40][15][0][80]\r\n"
-				+ "[15][40][65][80][0]\r\n";
+				"[0][30][55][70][15]\n"
+				+ "[30][0][25][40][40]\n"
+				+ "[55][25][0][15][65]\n"
+				+ "[70][40][15][0][80]\n"
+				+ "[15][40][65][80][0]\n";
 		AlgoritmoFloyd algoritmo = new AlgoritmoFloyd();
 		Scanner scanner = new Scanner(System.in);
 		//Solicitud de la ruta del archivo.
@@ -40,6 +41,26 @@ class GraphTest {
 		Graph matriz = reader.leerTxt(ruta);
 		ArrayList<String>respuestas = algoritmo.algoritmoFloyd(ciudades, matriz);
 		String valorRecibido = respuestas.get(0);
+		assertEquals(valorEsperado, valorRecibido);	
+	}
+	
+	@Test
+	void centerGraph() {
+		String valorEsperado = "Antigua";
+		AlgoritmoFloyd algoritmo = new AlgoritmoFloyd();
+		Scanner scanner = new Scanner(System.in);
+		//Solicitud de la ruta del archivo.
+		System.out.println("Ingrese la ruta en la que se encuentra el archivo .txt a escanear.");
+		String ruta = scanner.nextLine();
+		
+		//Instancia de la clase que escaneara ListadoProducto.txt
+		Reader reader = new Reader();
+		ArrayList<String> ciudades = reader.obtenerCiudades(ruta);
+		Graph matriz = reader.leerTxt(ruta);
+
+		Controller controlador = new Controller(ciudades, matriz);
+		String valorRecibido = controlador.centerGraph();
+		
 		assertEquals(valorEsperado, valorRecibido);	
 	}
 
